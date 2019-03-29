b1dba151a72b4de45a364538bf4bbf8a09c6ae01
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-29 11:37:16 +0900

    add Comment model

--- posts/models.py
+++ posts/models.py
@@ -4,4 +4,6 @@ class Article(models.Model):
     title_text = models.CharField(max_length=30)  
     content_text = models.CharField(max_length=140)
 
-
+class Comment(models.Model):
+    content_text = models.CharField(max_length=50)
+    
