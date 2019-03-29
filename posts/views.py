b8696a9d15e3ab15c580d8da814204cee399ff30
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-29 10:57:43 +0900

    using template/posts/index.html

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Article
 
 def index(request):
-    article_list = Article.objects
-    return HttpResponse(article_list)
+    article_list = Article.objects.all()
+    template = loader.get_template('posts/index.html')
+    context = {
+        'article_list': article_list,
+    }
+    return HttpResponse(template.render(context, request))
     
