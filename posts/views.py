4d7210c7230a55fd54015705929013a93eaadb01
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-29 10:49:53 +0900

    send Article model to /posts/

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Article
+
+def index(request):
+    article_list = Article.objects
+    return HttpResponse(article_list)
+    
