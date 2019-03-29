df1cf382caec5ef43f01476b1c3a54c77649246f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-29 10:59:13 +0900

    using render shortcuts

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Article
 
 def index(request):
     article_list = Article.objects.all()
-    template = loader.get_template('posts/index.html')
     context = {
         'article_list': article_list,
     }
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(render(request, 'posts/index.html', context))
     
