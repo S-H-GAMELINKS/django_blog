53f57feeeaeccf47fc0b57835b2ffe5e2e03e814
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-01 10:52:53 +0900

    reference article id in comment

--- posts/models.py
+++ posts/models.py
@@ -5,5 +5,6 @@ class Article(models.Model):
     content_text = models.CharField(max_length=140)
 
 class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, default='')
     content_text = models.CharField(max_length=50)
     
